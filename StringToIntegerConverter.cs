65db8da958e19a71e841cd4c25ecb327116aa3bb
Aaron Anderson <Aaron_Anderson@starkey.com>
2020-09-10 13:43:16 -0500

    initial-commit

---
+++ StringToIntegerConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfApp1
+{
+	internal class IntegerToStringConverter : IValueConverter
+	{
+
+		#region Methods
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return value.ToString();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			string input = value as string;
+
+			int output;
+
+			if (int.TryParse(input, out output))
+				return output;
+			else
+				return DependencyProperty.UnsetValue;
+		}
+
+		#endregion Methods
+
+	}
+}
---
+++ ViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace WpfApp1
+{
+	public class ViewModel : INotifyPropertyChanged
+	{
+
+		#region Fields
+
+		private string _message;
+		private int _outNumber = 0;
+
+		#endregion Fields
+
+		#region Constructors
+
+		public ViewModel(string message)
+		{
+			_message = message;
+		}
+
+		public ViewModel()
+		{
+			_message = "Default message";
+		}
+
+		#endregion Constructors
+
+		#region Events
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		#endregion Events
+
+		#region Properties
+
+		public String Message
+		{
+			get => _message;
+			set
+			{
+				_message = value;
+				OnPropertyChanged();
+			}
+		}
+		public int OutNumber
+		{
+			get => _outNumber;
+			set
+			{
+				_outNumber = value;
+				OnPropertyChanged();
+			}
+		}
+
+		#endregion Properties
+
+		#region Methods
+
+		protected void OnPropertyChanged([CallerMemberName] string propertySource = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertySource));
+		}
+
+		#endregion Methods
+
+	}
+}
